592c8042da686c79e84907e962e6cc94342a8673
Luuhoaaa1212 <hahahawe1239@gmail.com>
2023-11-12 19:37:55 +0700

    update item

---
+++ index.js
@@ -0,0 +1,25 @@
+const arr  = [
+    {
+        name:"hoa1"
+    },
+    {
+        name:"hoa2"
+    },
+    {
+        name:"hoa3"
+    },
+    {
+        name:"hoa4"
+    },
+    {
+        name:"hoa5"
+    },
+
+    {
+        name:"hoa6"
+    },
+    {
+        name:"hoa7"
+    },
+]
+
